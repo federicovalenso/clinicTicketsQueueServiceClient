744ed62798c80dd53f4e5107b44c14aac63a9225
VVF90 <federicovalenso@gmail.com>
2020-02-12 14:06:55 +0800

    Behavior and style Added switching to background state Indentation fixes

--- states/nextticketstate.cpp
+++ states/nextticketstate.cpp
@@ -3,14 +3,18 @@
 
 using namespace vvf;
 
-NextTicketState *NextTicketState::getInstance()
-{
-    static NextTicketState instance;
-    return &instance;
+NextTicketState *NextTicketState::getInstance() {
+  static NextTicketState instance;
+  return &instance;
 }
 
-void NextTicketState::processTickets(MainWindow *w, const QVector<Ticket>& tickets)
-{
-    auto oldestTicket = std::min_element(tickets.begin(), tickets.end());
-    lockTicket(w, *oldestTicket);
+void NextTicketState::processTickets(MainWindow *w,
+                                     const QVector<Ticket> &tickets) {
+  auto oldestTicket = std::min_element(tickets.begin(), tickets.end());
+  lockTicket(w, *oldestTicket);
+}
+
+void NextTicketState::error(MainWindow *w, const QString &error) {
+  AppState::error(w, error);
+  changeState(w, BackgroundState::getInstance());
 }
